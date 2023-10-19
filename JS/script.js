d7a1cdca22fb6b5f7da4bb44506ec2210cbba65b
Mehrab83 <mehrabmahmud2002@gmail.com>
2023-10-19 20:38:41 +0600

    add all filoe and folder

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+$(function(){
+  $(function(){
+
+    $('.dest_slide').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      arrows: false,
+      centerMode: true,
+      centerPadding: 0,
+    });
+  
+  })
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+$('.explore_slide').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  arrows: false,
+  centerMode: true,
+  centerPadding: 0,
+  dots: true,
+});
+
+})
